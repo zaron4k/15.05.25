32c5e07434fe24ebbc92e053d08d2f9c2d1fc2c0
zaron4k <76218026+zaron4k@users.noreply.github.com>
2025-05-15 11:05:12 +0300

    Add files via upload
    1)                 В классе Program создайте 2 статических метода:
    
    -                    метод принимает два вещественных числа и возвращает наименьшее из них;
    
    -                    метод принимает два вещественных числа и возвращает наибольшее из них.
    
    2)                 Объявите тип делегата, соответствующий этим методам.
    
    3)                 Объявите сам делегат.
    
    4)                 Примените созданный делегат.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+
+        public static double MinNumber(double a, double b)
+        {
+            return a < b ? a : b;
+        }
+        public static double MaxNumber(double a, double b)
+        {
+            return a > b ? a : b;
+        }
+        delegate double CompareNumbers(double a, double b);
+        static void Main(string[] args)
+        {
+            CompareNumbers minNumber = new CompareNumbers(MinNumber);
+
+            CompareNumbers maxNumber = new CompareNumbers(MaxNumber);
+
+            double num1 = 3.7;
+            double num2 = 5.6;
+            Console.WriteLine("Наименьшое число :" + minNumber(num1,num2));
+            Console.WriteLine("Наибольшее число :" + maxNumber(num1,num2));
+            Console.Read();
+        }
+    }
+}
